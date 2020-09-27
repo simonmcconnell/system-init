5cf61b7aacefb55a6d758d048f6cc6c606d7687e
Simon McConnell <22566656+simonmcconnell@users.noreply.github.com>
2020-09-27 16:31:49 +1000

    Update setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -38,3 +38,18 @@ sudo Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsys
 # download config files
 curl https://raw.githubusercontent.com/simonmcconnell/system-init/master/windows/.gitconfig --output $HOME\.gitconfig
 curl https://raw.githubusercontent.com/simonmcconnell/system-init/master/windows/settings.json --output "$env:LOCALAPPDATA\Microsoft\Windows Terminal\settings.json"
+
+# set 7zip file associations
+Write-Host -ForegroundColor Blue ">>> 7zip <<<"
+Write-Host -ForegroundColor Blue "run 7zFM.exe as administrator and:"
+Write-Host -ForegroundColor Blue "- set file associations in Tools > Options"
+Write-Host -ForegroundColor Blue "- on 7-Zip tab, check 'Integrate 7-Zip to shell context menu' and deselect unwanted context menu items"
+Write-Host -ForegroundColor Blue "- on Editor tab, set all to vscode (likely C:\Users\simon\AppData\Local\Programs\Microsoft VS Code\Code.exe)"
+
+sudo $HOME\scoop\apps\7zip\current\7zFM.exe
+
+# $exts = '7z','zip','rar','001','xz','txz','lzma','tar','cpio','bz2','bzip2','tbz2','tbz','gz','gzip','tgz','tpz','z','taz','lzh','lhz','rpm','dep','arj','wim','swm','fat','ntfs','dmg','hfs','xar','squashfs'
+# ForEach ($ext in $etxs) {
+#    sudo cmd /c "assoc .$ext=7-Zip.$ext"
+#    sudo cmd /c ftype 7-Zip.$ext=$HOME\scoop\apps\7-Zip\current\7zFM.exe
+#}
