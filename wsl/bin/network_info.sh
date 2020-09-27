e06a26217d041f781cce157da58b582d7dc07c80
Simon McConnell <22566656+simonmcconnell@users.noreply.github.com>
2020-09-28 09:04:33 +1000

    Create network_info.sh

---
+++ wsl/bin/network_info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if $(tasklist.exe | grep 'obs' -q) || ! $(tmux show-option -gqv @show-network); then
+  echo -n "🏠 #[fg=colour197]192.168.0.0 #[fg=black]|#[fg=colour197] 📡 255.255.255.256"
+else
+  # Internal IP
+  IP=$(hostname -I | awk {'print $1}')
+
+  PUBLIC_IP=$(curl -4 ifconfig.co)
+
+  if [[ "$PUBLIC_IP" = ";; connection timed out; no servers could be reached" ]]; then 
+      PUBLIC_IP="Not Available"
+  elif [[ "$PUBLIC_IP" = "" ]]; then
+      PUBLIC_IP="No external access"
+  else
+      PUBLIC_IP=$(curl -4 ifconfig.co)
+  fi
+
+  echo -n "🏠 #[fg=colour197]$IP #[fg=black]|#[fg=colour197] 📡 $PUBLIC_IP"
+fi
