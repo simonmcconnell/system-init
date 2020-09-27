7393a1b2f9e0ed7bc7e3ff91e740a91d3b076e35
Simon McConnell <22566656+simonmcconnell@users.noreply.github.com>
2020-09-27 15:37:19 +1000

    Update setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,40 +1,9 @@
-# function CreateRefresenv {
-#     Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://github.com/chocolatey/choco/raw/stable/src/chocolatey.resources/helpers/functions/Update-SessionEnvironment.ps1')
-# }
-
 function Install-Scoop {
     scoop --version 
     if ($LastExitCode -ne 0)
     {
         Set-ExecutionPolicy RemoteSigned -scope CurrentUser
         Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-    }
-}
-
-function Install-Fonts {
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]$FontFile
-    )
-    try {
-        $font = $fontFile | split-path -Leaf
-        If (!(Test-Path "c:\windows\fonts\$($font)")) {
-            switch (($font -split "\.")[-1]) {
-                "TTF" {
-                    $fn = "$(($font -split "\.")[0]) (TrueType)"
-                    break
-                }
-                "OTF" {
-                    $fn = "$(($font -split "\.")[0]) (OpenType)"
-                    break
-                }
-            }
-            Copy-Item -Path $fontFile -Destination "C:\Windows\Fonts\$font" -Force
-            New-ItemProperty -Name $fn -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $font
-        }
-    }
-    catch {
-        write-warning $_.exception.message
     }
 }
 
@@ -49,10 +18,13 @@ scoop install pwsh posh-git oh-my-posh
 scoop install 7zip coreutils curl less sudo 
 
 # devtools
-scoop install git vscode vcredist2019 windows-terminal vcxsrv openssh
+scoop install git vcredist2019 windows-terminal vcxsrv openssh # vscode
+
+# add vscode to context menus
+# $HOME\scoop\apps\current\vscode-install-context.reg
 
 # languages
-# erlang install is failing with 'ERROR Exit code was -1073741515!' if vcredist2013 is not installed, which includes MSVCR120.dll
+# erlang install fails with 'ERROR Exit code was -1073741515!' if vcredist2013 is not installed, which includes MSVCR120.dll
 scoop install vcredist2013 erlang@23.1 elixir python nodejs-lts
 
 # apps
