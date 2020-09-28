14e48d1751c984ef2b9fdffbdebcde6f21d488a2
Simon McConnell <22566656+simonmcconnell@users.noreply.github.com>
2020-09-28 11:51:34 +1000

    Update setup.sh

--- wsl/setup.sh
+++ wsl/setup.sh
@@ -45,14 +45,15 @@ install_devtools() {
     ## asdf
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
     echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
-
+    source ~/.bashrc
+    
     ## erlang
     sudo apt -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
     export KERL_BUILD_DOCS=yes
 
     asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
     asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-    asdf plugin-add nodejs git@github.com:asdf-vm/asdf-nodejs.git
+    asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
     asdf plugin-add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git
     asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
     asdf plugin-add python
